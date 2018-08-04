3dee0146a7e9f8510aa925c36a99f99ba8540ce5
Jakub Marek <jakub@Jakubs-MacBook-Pro.local>
2018-08-04 22:51:31 +0200

    + LoginVC UI

---
+++ ChatApp/Controller/LoginVC.swift
@@ -0,0 +1,22 @@
+//
+//  LoginVC.swift
+//  ChatApp
+//
+//  Created by Jakub Marek on 04/08/2018.
+//  Copyright © 2018 Jakub Marek. All rights reserved.
+//
+
+import UIKit
+
+class LoginVC: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+    }
+    
+    
+    @IBAction func closePressed(_ sender: Any) {
+        dismiss(animated: true, completion: nil)
+    }
+}
---
+++ ChatApp/Utilities/Constants.swift
@@ -0,0 +1,12 @@
+//
+//  Constants.swift
+//  ChatApp
+//
+//  Created by Jakub Marek on 04/08/2018.
+//  Copyright © 2018 Jakub Marek. All rights reserved.
+//
+
+import Foundation
+
+// Segues
+let TO_LOGIN = "toLogin"
