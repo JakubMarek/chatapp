536a7549602d0f296d6a160b3f9ab5610a7a5797
Jakub Marek <jakub@Jakubs-MacBook-Pro.local>
2018-08-04 22:21:21 +0200

    + ChannelVC UI

---
+++ ChatApp/Controller/ChatVC.swift
@@ -0,0 +1,24 @@
+//
+//  ChatVC.swift
+//  ChatApp
+//
+//  Created by Jakub Marek on 04/08/2018.
+//  Copyright © 2018 Jakub Marek. All rights reserved.
+//
+
+import UIKit
+
+class ChatVC: UIViewController {
+
+    // Outlets
+    @IBOutlet weak var menuBtn: UIButton!
+    
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        menuBtn.addTarget(self.revealViewController(), action: #selector(SWRevealViewController.revealToggle(_:)), for: .touchUpInside)
+        self.view.addGestureRecognizer(self.revealViewController().panGestureRecognizer())
+        self.view.addGestureRecognizer(self.revealViewController().tapGestureRecognizer())
+    }
+}
+
