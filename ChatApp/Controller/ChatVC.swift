efec4dbeb51361a2f52e6fe1db7aba92abc4e484
Jakub Marek <jakub@Jakubs-MacBook-Pro.local>
2018-09-08 18:38:12 +0200

    Login done

--- ChatApp/Controller/ChatVC.swift
+++ ChatApp/Controller/ChatVC.swift
@@ -13,12 +13,17 @@ class ChatVC: UIViewController {
     // Outlets
     @IBOutlet weak var menuBtn: UIButton!
     
-    
     override func viewDidLoad() {
         super.viewDidLoad()
         menuBtn.addTarget(self.revealViewController(), action: #selector(SWRevealViewController.revealToggle(_:)), for: .touchUpInside)
         self.view.addGestureRecognizer(self.revealViewController().panGestureRecognizer())
         self.view.addGestureRecognizer(self.revealViewController().tapGestureRecognizer())
+        
+        if AuthService.instance.isLoggedIn {
+            AuthService.instance.findUserByEmail { (success) in
+                NotificationCenter.default.post(name: NOTIF_USER_DATA_DID_CHANGED, object: nil)
+            }
+        }
     }
 }
 
--- ChatApp/Controller/LoginVC.swift
+++ ChatApp/Controller/LoginVC.swift
@@ -10,9 +10,35 @@ import UIKit
 
 class LoginVC: UIViewController {
 
+    // Outlets
+    @IBOutlet weak var usernameText: UITextField!
+    @IBOutlet weak var passwordTxt: UITextField!
+    @IBOutlet weak var spinner: UIActivityIndicatorView!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-
+        setupView()
+    }
+    
+    @IBAction func loginPressed(_ sender: Any) {
+        spinner.isHidden = false
+        spinner.startAnimating()
+        
+        guard let email = usernameText.text , usernameText.text != "" else{return}
+        guard let pass = passwordTxt.text , passwordTxt.text != "" else{return}
+        
+        AuthService.instance.loginUser(email: email, password: pass) { (success) in
+            if success{
+                AuthService.instance.findUserByEmail(completion: { (success) in
+                    if success{
+                        NotificationCenter.default.post(name: NOTIF_USER_DATA_DID_CHANGED, object: nil)
+                        self.spinner.isHidden = true
+                        self.spinner.stopAnimating()
+                        self.dismiss(animated: true, completion: nil)
+                    }
+                })
+            }
+        }
     }
     
     @IBAction func closePressed(_ sender: Any) {
@@ -21,5 +47,11 @@ class LoginVC: UIViewController {
     
     @IBAction func createAccPressed(_ sender: Any) {
         performSegue(withIdentifier: TO_CREATE_ACCOUNT, sender: nil)
+    }
+    
+    func setupView(){
+        spinner.isHidden = true
+        usernameText.attributedPlaceholder = NSAttributedString(string: "username", attributes: [NSAttributedStringKey.foregroundColor: smackPurplePlaceholder])
+        passwordTxt.attributedPlaceholder = NSAttributedString(string: "password", attributes: [NSAttributedStringKey.foregroundColor: smackPurplePlaceholder])
     }
 }
